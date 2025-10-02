2ef316ecbb3352c499ab29ce672ffbd83a57dcf2
sliitguy <it23259416@my.sliit.lk>
2025-10-02 15:50:32 +0530

    updated changes in files

--- server.js
+++ server.js
@@ -1,37 +1,36 @@
 const http = require('http')
+const app = require('./src/app')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
-const app = require('./src/app')
-
-const server = http.createServer(app)
 
 dotenv.config()
 
 const PORT = process.env.PORT || 8002
-const MONGODB_URI = process.env.MONGODB_URI 
 
 mongoose.connection.once('open', () => {
     console.log("MongoDB is ready!")
 })
 
 mongoose.connection.on('error', () => {
-    console.error("Error in connecting with MongoDB!")
+    console.error('Error in connecting with MongoDB')
 })
 
+const server = http.createServer(app)
 
 async function createServer() {
     try {
-        mongoose.connect(MONGODB_URI)
-        console.log("Connected to MongoDB")
-
+        
         server.listen(PORT, () => {
-            console.log(`Parcel Service: Listening on port ${PORT}`)
+            console.log(`Server listening on port ${PORT}..`)
         })
 
+        
     } catch(err) {
-        console.error("Parcel Service: Internal Server error: ", err)
+        console.error('Internal server error: ', err)
         process.exit(1)
     }
 }
 
 createServer()
+
+
--- src/app.js
+++ src/app.js
@@ -1,7 +1,25 @@
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
 
+app.use(cors({
+    origin: [
+        'https://sparrow.nivakaran.dev',
+        'http://localhost:3000',
+        'http://nivakaran.dev'
+    ]
+}))
+
 app.use(express.json())
 
-module.exports = app;
+app.get('/', (req, res) => {
+    res.json({message: "Sparrow: Consolidation Service"})
+})
+
+app.get('/health', (req, res) => {
+    res.json({message: "Consolidation Service is running.."})
+})
+
+
+module.exports = app
